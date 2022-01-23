5722ff0b5404c7aece030371456729c93ad11566
Horn <horn@mmbbs.de>
2022-01-23 13:13:59 +0100

    Beispiel mit Bild und Code

---
+++ Tier.java
@@ -0,0 +1,20 @@
+package docs;
+
+public class Tier {
+    private String name;
+    private double gewicht;
+
+    public Tier(String name)
+    {
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getGewicht(){
+        return gewicht;
+    }
+    public void setGewicht(double gewicht){
+        this.gewicht=gewicht;
+    }
+}
